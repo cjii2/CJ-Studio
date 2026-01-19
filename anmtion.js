870a9228ad40480126187dc7a5253e5df77de987
cjii2 <ahmed12345m3@gmail.com>
2026-01-19 17:31:42 +0300

    update

--- anmtion.js
+++ anmtion.js
@@ -8,8 +8,8 @@ class videoAnimationScroll {
     }
     activeANM(){
         this.mm.add({
-            isMobile:"(max-width:500px)",
-            isDesktop:"(min-width: 501px)"
+            isMobile:"(max-width:700px)",
+            isDesktop:"(min-width: 701px)"
         },(context) =>{
             let {isMobile , isDesktop} = context.conditions;
 
@@ -27,7 +27,7 @@ class videoAnimationScroll {
             opacity:0,
             duration: 1,
             stagger: 0.2
-        })
+        });
 
         gsap.from(this.aboutme, {
             y: 40,
@@ -37,15 +37,56 @@ class videoAnimationScroll {
             scrollTrigger: {
                 trigger: "#about-me",
                 start: "0% 95%",
+                end: '100% 40%',
+                scrub:true
             }
         });
 
+        gsap.from(`#Exhibition-section .card , #Exhibition-section h1, #Exhibition-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#Exhibition-section",
+                start: "0% 95%",
+                end: "50% 60%",
+                scrub:true,
+                // markers:true
+            }
+        });
+
+        gsap.from(`#services-section .card , #services-section h1, #services-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#services-section",
+                start: "0% 95%",
+                end: "100% 96%",
+                scrub:true,
+            }
+        });
+        
+        gsap.from(`#contect-me-section section , #contect-me-section h1, #contect-me-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#contect-me-section",
+                start: "0% 95%",
+                end: "100% 96%",
+                scrub:true,
+            }
+        });
     }
 
     mobile() {
         if (!this.aboutme || this.aboutme.length === 0) return;
         if (!this.HomeElemnt || this.HomeElemnt.length === 0) return;
-        this.tl.from(this.headerElemnt, {
+        this.tl.from('#openSideBar', {
             x: -40,
             opacity: 0,
             duration: 1,
@@ -62,6 +103,46 @@ class videoAnimationScroll {
                 start: "0% 95%",
                 end: "bottom bottom",
                 scrub: true,
+            }
+        });
+
+        gsap.from(`#Exhibition-section .card , #Exhibition-section h1, #Exhibition-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#Exhibition-section",
+                start: "0% 95%",
+                end: "100% 80%",
+                scrub:true,
+            }
+        });
+
+        gsap.from(`#services-section .card , #services-section h1, #services-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#services-section",
+                start: "top 95%",
+                end: "100% 96%",
+                // scrub:true,
+                // markers:true
+            }
+        });
+        
+        gsap.from(`#contect-me-section section , #contect-me-section h1, #contect-me-section p`,{
+            y: 40,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#contect-me-section",
+                start: "0% 95%",
+                end: "100% 96%",
+                scrub:true,
             }
         });
     }
