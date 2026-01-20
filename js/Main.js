a229f792d0f0acdcc33c41e3a3edd520919f4abd
cjii2 <ahmed12345m3@gmail.com>
2026-01-20 19:09:07 +0300

    update

--- js/Main.js
+++ js/Main.js
@@ -1,15 +1,5 @@
 const OpensideBar = document.getElementById("openSideBar");
 const closeSideBar = document.getElementById("closeSideBar");
-function videoChange(){
-    const vd = document.getElementById('video');
-    const WindowWidth = window.innerWidth
-    
-    if(WindowWidth < 700){
-        vd.src = "video/videoWebFilming.mp4"
-    }else{
-        vd.src = "video/potoPro.mp4"
-    }
-}
 
 OpensideBar.addEventListener('click',()=>{
     document.getElementById("navbar-section").classList.add("active");
@@ -20,5 +10,3 @@ closeSideBar.addEventListener('click',()=>{
     document.getElementById("navbar-section").classList.remove("active");
     OpensideBar.classList.remove("hide")
 })
-videoChange();
-window.addEventListener('resize',videoChange);
